2e62aba698663834a708cddc1ed68b5a335307a0
Robin Devkota <robin.devkota@amniltech.com>
2025-05-18 10:51:13 +0545

    cors

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ const app = express()
 
 app.use(cookieParser())
 app.use(express.json())
-app.use(cors({
+app.options('*', cors({
     origin: process.env.FRONTEND_URL,
     credentials: true
 }))
