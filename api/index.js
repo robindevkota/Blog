946bcaa3965166c8a45bb00a6c31f3618bda5976
Robin Devkota <robin.devkota@amniltech.com>
2025-05-18 11:04:05 +0545

    fix

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ const app = express()
 app.use(cookieParser())
 app.use(express.json())
 app.options('*', cors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL || 'https://blog-fornt.netlify.app',
     credentials: true
 }))
 
